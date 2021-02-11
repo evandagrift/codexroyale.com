c710abb2ba85873d256de51ede2b8edaaab63426
unknown <vandagrifte@my.lanecc.edu>
2021-02-11 00:38:29 -0800

    Clash Royale Newtonsoft Classes

---
+++ RoyaleTrackerClasses/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoyaleTrackerClasses
+{
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+
+    }
+}
