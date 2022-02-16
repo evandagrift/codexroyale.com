3d1bef46a56abb2570bd084322ef91a702d86c5f
evandagrift <evandagrift@gmail.com>
2022-02-16 06:07:15 -0800

    .ignore updated

--- react.js frontend/src/axios.js
+++
@@ -1,9 +0,0 @@
-import Axios from "axios";
-const localURL = "//localhost:52003/api/"
-let headers = {};
-if (localStorage.user){
-   headers.Authorization = `bearer ${localStorage.user['token']}`;
-}
-export const axios = Axios.create({baseURL:localURL,
-headers,
-});
